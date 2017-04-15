abe5377d6f843f3520d3fb224ec68da7a7c5b4e9
Damian Beresford <dberesford@gmail.com>
2017-04-15 09:07:27 +0100

    Exception handling

--- db.cc
+++ db.cc
@@ -6,23 +6,31 @@
 
 using namespace rocksdb;
 using namespace std;
+using namespace v8;
 
 NAN_METHOD(OpenDB) {
-  string path = string(*Nan::Utf8String(info[0]));
-  cout << "creating: " << path;
+  Isolate* isolate = info.GetIsolate();
+  if (info.Length() < 1) {
+    isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, "Wrong number of arguments")));
+    return;
+  }
 
+  string path = string(*Nan::Utf8String(info[0]));
   DB* db;
 
-  // TODO - pass options in..
-  
+  // TODO - pass options in as param..
   Options options;
   options.create_if_missing = true;
+  //options.error_if_exists = true;
 
-  // open DB
   Status s = DB::Open(options, path, &db);
-  if (!s.ok()) cerr << s.ToString() << endl;
+  if (!s.ok()) {
+    isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, s.getState())));
+    return;
+  }
 
+  Local<Object> obj = Object::New(isolate);
+  obj->Set(String::NewFromUtf8(isolate, "msg"), info[0]->ToString());
 
-  // TODO!!
-  info.GetReturnValue().Set(10);
+  info.GetReturnValue().Set(obj);
 }
--- test/basic.test.js
+++ test/basic.test.js
@@ -1,5 +1,15 @@
+// var assert = require('assert')
 var tap = require('tap')
+var test = require('tap').test
 
 var rocksdb = require('../build/Release/rocksdb.node')
 var db = rocksdb.open('/tmp/rockme')
 tap.ok(db)
+
+test('exception thrown for wrong args', function (t) {
+  t.throws(function () {
+    rocksdb.open()
+  }, /Wrong number of arguments/)
+
+  t.end()
+})
